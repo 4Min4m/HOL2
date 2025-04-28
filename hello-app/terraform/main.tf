9bac969cba1d92deb5c5c5e6979efde0e941f756
Amin <36745993+4Min4m@users.noreply.github.com>
2025-04-28 19:18:18 +0000

    first update

--- hello-app/terraform/main.tf
+++ hello-app/terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "myhellobucket-132"
+    key            = "hello-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
